85fff944e98aeca20a4002f0ac88496607bcbda4
Nick Ng <nicholas.cj.ng@gmail.com>
2017-06-10 21:00:06 +1200

    Made initial contentful service module
    Also updated npm and some packages

---
+++ src/config/prod.js
@@ -0,0 +1,4 @@
+export default {
+  serverUrl: 'http://localhost:4000',
+  contentfulProxyUrl: 'https://nick-gbh-contentful-proxy-s.herokuapp.com',
+};
--- src/services/contentful.js
+++ src/services/contentful.js
@@ -1,9 +1,10 @@
 import utils from '../utils';
-// import config from '../config';
+import config from '../config';
 
-const getAllEntries = async (spaceId = 'asdf') => {
-  const allEntries = await utils.get(`https://cdn.contentful.com/spaces/${spaceId}/entries?access_token=asdf`);
-  console.log('allEntries', allEntries);
+const { contentfulProxyUrl } = config;
+
+export const getAllEntries = async () => {
+  const allEntries = await utils.get(`${contentfulProxyUrl}/player-list`);
   return allEntries;
 };
 
